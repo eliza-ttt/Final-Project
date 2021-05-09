fadb9428af2a239467ef4fb437ce873927f1f05a
eliza-ttt <73555386+eliza-ttt@users.noreply.github.com>
2021-05-09 20:30:46 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,28 @@
+package no.dremar17.assignment2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setFullScreen(true);
+        primaryStage.setTitle("quiz");
+        primaryStage.setScene(new Scene(root, 470, 390));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
